141de897f09029c675d3cf633214dacd4cee9d4c
joshb <joshberenguel@gmail.com>
2017-12-25 10:02:24 -0800

    Initial Commit

---
+++ core/src/com/joshb/game/Screens/PlayScreen.java
@@ -0,0 +1,62 @@
+package com.joshb.game.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.joshb.game.MarioBros;
+
+/**
+ * Created by joshb on 12/24/2017.
+ */
+
+public class PlayScreen implements Screen {
+    private MarioBros game;
+    Texture texture;
+    private OrthographicCamera gameCam;
+
+    public PlayScreen(MarioBros game) {
+        this.game = game;
+        texture = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.begin();
+        game.batch.draw(texture, 0, 0);
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ desktop/src/com/joshb/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.joshb.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.joshb.game.MarioBros;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MarioBros(), config);
+	}
+}
